a46ba2b243938fdef5160564dbee52ec88fabb22
tanhuynh4019 <98221135+tanhuynh4019@users.noreply.github.com>
2022-05-15 02:05:14 +0700

    cấu hình

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar app color="#004D40" height="70">
+      <!-- <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon> -->
+
+      <v-spacer>
+        <v-container>
+          <v-row>
+            <v-col cols="3">
+              <img
+                src="https://templates.hibootstrap.com/jecto/default/assets/img/logo-three.png"
+              />
+            </v-col>
+            <v-col cols="9"> Menu </v-col>
+          </v-row>
+        </v-container>
+      </v-spacer>
+
+      <v-responsive max-width="300">
+        <div>Login/ Signup <v-btn>Tìm ứng viên</v-btn></div>
+      </v-responsive>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main class="grey lighten-2">
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({ drawer: null }),
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 1200px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    Trang chủ
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: "Home",
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 1500px;
+}
+</style>
